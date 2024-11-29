2789c31bba52810b1ec4ee57a7839814506fc8f0
sathwik kd <skadasur@gmail.com>
2024-11-29 09:36:58 +0530

    updated

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,11 @@
-fn main() {}
+accessing tuple by index...
+// fn main() {
+//     let tup: (i32, u32, f32) = (900, 90, 9.0);
+//     println!("{}\n {}\n {}", tup.0, tup.1, tup.2)
+// }
+accessing tuple by destructuring...
+fn main() {
+    let tup = (10, 29.8, true);
+    let (a, b, c) = tup;
+    println!("{}, {}, {}", a, b, c)
+}
