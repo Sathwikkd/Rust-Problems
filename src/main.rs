e6969d1f9cf627d9d97e58b9cb59d202024cd094
sathwik kd <skadasur@gmail.com>
2024-12-07 10:02:11 +0530

    updated

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,7 @@
-fn main() {}
+fn main() {
+    let str1: String = String::from("this is kd");
+    println!("{}", str1);
+    let str2 = str1.clone();
+    println!("{str1}");
+    println!("{str2}")
+}
