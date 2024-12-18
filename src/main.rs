d498f13407c87b5699bacaf39b4329a46e688db0
sathwik kd <skadasur@gmail.com>
2024-12-18 18:34:19 +0530

    updated

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,1 @@
-fn main(){
-    let mut s1:String=String::from("hi skd");
-    let res=len_finder(&mut s1);
-    println!("length of the string is -> {}",res);
-}
-
-fn len_finder(s2:& mut String)->usize{
-    s2.len()
-
-}
+fn main() {}
