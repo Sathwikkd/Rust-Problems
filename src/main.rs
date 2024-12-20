350d725f142b37b892ae5f05285abff0f339ce9d
sathwik kd <skadasur@gmail.com>
2024-12-20 22:40:14 +0530

    array added

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,5 @@
-fn main() {}
+fn main() {
+    let arr:[i32;10]=[10;10]; //declaring and initializing elements to an array
+    let arr1=&arr[0..3];  //array slicing [0..3]
+    println!("{:?}",arr1);
+}
