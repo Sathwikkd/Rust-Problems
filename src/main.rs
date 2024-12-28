4d7f22417c6ac5d00c349eb5771b79f0456c935c
sathwik kd <skadasur@gmail.com>
2024-12-28 11:33:46 +0530

    read ele into arr

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,15 @@
-fn main() {}
+use std::io;
+
+fn main() {
+    let mut arr = [0; 5];
+    println!("enter 5 elementss to array..");
+    for i in 0..arr.len() {
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect(
+            "failed to read..
+            ",
+        );
+        arr[i] = input.trim().parse().expect("failed to parse");
+    }
+    println!("{arr:?}")
+}
