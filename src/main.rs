31f3f88a14cc2b369086a42acf78f7028a2ff6a0
sathwik kd <skadasur@gmail.com>
2024-12-29 22:39:46 +0530

    updated

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,7 @@
-pub trait Eat {
-    fn eatdinner(&self) {
-        println!("i eat my dish")
-    }
-}
-
-pub struct Cat {
-    dinner: String,
-}
-
-pub struct Dog {
-    has_dinner: bool,
-}
-
-impl Eat for Cat {
-    fn eatdinner(&self) {
-        // println!("cat eats rat!");
-    }
-}
-impl Eat for Dog {
-    // fn eatdinner(&self) {
-    //     // println!("dog had dinenr..")
-    // }
-}
-
 fn main() {
-    let eat1 = Cat {
-        dinner: String::from("had dinenr.."),
-    };
-
-    let eat2 = Dog { has_dinner: true };
-    // eat1.eatdinner();
-    eat2.eatdinner();
+    let vec = vec![1, 2, 3, 4, 5];
+    let iter = vec.into_iter();
+    for val in iter {
+        println!("{val}")
+    }
 }
