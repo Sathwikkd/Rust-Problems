112682d73fbbcb7c2cf997342fa20e9a6a69d490
sathwik kd <skadasur@gmail.com>
2024-12-29 21:37:57 +0530

    no issue

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,34 @@
-use std::io;
+pub trait Eat {
+    fn eatdinner(&self) {
+        println!("i eat my dish")
+    }
+}
+
+pub struct Cat {
+    dinner: String,
+}
+
+pub struct Dog {
+    has_dinner: bool,
+}
+
+impl Eat for Cat {
+    fn eatdinner(&self) {
+        // println!("cat eats rat!");
+    }
+}
+impl Eat for Dog {
+    // fn eatdinner(&self) {
+    //     // println!("dog had dinenr..")
+    // }
+}
 
 fn main() {
-    let mut arr = [0; 5];
-    println!("enter 5 elementss to array..");
-    for i in 0..arr.len() {
-        let mut input = String::new();
-        io::stdin().read_line(&mut input).expect(
-            "failed to read..
-            ",
-        );
-        arr[i] = input.trim().parse().expect("failed to parse");
-    }
-    println!("{arr:?}")
+    let eat1 = Cat {
+        dinner: String::from("had dinenr.."),
+    };
+
+    let eat2 = Dog { has_dinner: true };
+    // eat1.eatdinner();
+    eat2.eatdinner();
 }
